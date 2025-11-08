beefca718d15845dfd0d4f240309bc6730497f05
taniyahlockley <tzlockley@aggies.ncat.edu>
2025-11-08 06:49:01 -0500

    Added Route Controller

--- ecoCodette/src/main/java/com/example/ecocodette/RouteController.java
+++ ecoCodette/src/main/java/com/example/ecocodette/RouteController.java
@@ -12,10 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
-
+//Handles requests for route and emission data.
+//DEfines API endpoints that return sample routes for Graingers eco routing system.
 @RestController
 public class RouteController {
-
+    //Sends back a list of sample routes
+    @GetMapping("/routes")
+    public List<Route> getRoutes() {
+        //DATA COMES FROM API DATABASE, connect with the API
+    }
 
 
 }
